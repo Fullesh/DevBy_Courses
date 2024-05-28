cbedec4c4d6877a132555d558b2e89e57c4b7afc
Danila Belov <danilabelov.adm@yandex.ru>
2024-05-28 18:57:15 +0300

    Добавлены пермишены

--- materials/serializers.py
+++ materials/serializers.py
@@ -21,3 +21,13 @@ class CourseSerializer(serializers.ModelSerializer):
         if instance.lession_set.all():
             return instance.lession_set.count()
         return 0
+
+    def create(self, validated_data):
+        lession = validated_data.pop('lession_set')
+
+        course_item = Course.objects.create(**validated_data)
+
+        for l in lession:
+            Lession.objects.create(**l, course=course_item)
+
+        return course_item
--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from users.models import Payment
+from users.models import Payment, User
 
 
 # Create your views here.
@@ -9,3 +9,26 @@ class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
