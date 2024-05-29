6329d61e83f662a029ff11824d944bbbff6bc4bd
Danila Belov <danilabelov.adm@yandex.ru>
2024-05-29 11:14:13 +0300

    Добавлен сериализатор подписки, добавлен метод get_subscription

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lession
+from materials.models import Course, Lession, Subscription
 from materials.validators import URLValidator
 
 
@@ -14,15 +14,21 @@ class LessionSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
     lession = LessionSerializer(source='lession_set', many=True)
     lessions_quantity = serializers.SerializerMethodField()
+    subscription = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
 
+    def get_subscription(self, instance):
+        user = self.context['request'].user
+        return Subscription.objects.all().filter(user=user).filter(course=instance).exists()
+
     def get_lessions_quantity(self, instance):
         if instance.lession_set.all():
             return instance.lession_set.count()
         return 0
+
 
     def create(self, validated_data):
         lession = validated_data.pop('lession_set')
@@ -33,3 +39,9 @@ class CourseSerializer(serializers.ModelSerializer):
             Lession.objects.create(**l, course=course_item)
 
         return course_item
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
