35d829bc21ef79423acaf2a6366a8f942fd91a9c
Danila Belov <danilabelov.adm@yandex.ru>
2024-05-30 19:29:05 +0300

    Реализованы контроллеры оплаты

--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters, generics
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from users.models import Payment, User
 from users.serializers import PaymentSerializer, CreateUserSerializer, UserSerializer
+from users.services import create_stripe_product, create_stripe_price, create_stripe_session
 
 
 # Create your views here.
@@ -43,3 +45,28 @@ class UserDestroyAPIView(generics.DestroyAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
+
+
+class PaymentsListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lession', 'payment_method')
+    ordering_fields = ('payment_date',)
+
+
+class PaymentsCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        payments = serializer.save()
+        payments.user = self.request.user
+        stripe_product_id = create_stripe_product(payments)
+        payments.amount = payments.payment_amount
+        price = create_stripe_price(stripe_product_id=stripe_product_id, amount=payments.amount)
+        session_id, payment_link = create_stripe_session(price=price)
+        payments.session_id = session_id
+        payments.link = payment_link
+        payments.save()
