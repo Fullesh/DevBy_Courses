55a00230d1e5b7935cbb67a4844cd44404e02cf9
Danila Belov <danilabelov.adm@yandex.ru>
2024-06-01 20:56:48 +0300

    Добавлена задача по отправке почты

---
+++ materials/tasks.py
@@ -0,0 +1,27 @@
+import smtplib
+
+from celery import shared_task
+from django.core.mail import send_mail
+
+from config import settings
+from materials.models import Course
+
+
+@shared_task
+def send_email_update(pk, user):
+    instance = Course.objects.get(pk=pk)
+    subscribe = instance.subscription.set.all().filter(user=user)
+    email_list = [user.email for subscribe.user in subscribe]
+    message_subject = f'Обновление курса {instance}'
+    message_text = f'Курс {instance} обновлён. Добавили много нового'
+    try:
+        server_response = send_mail(
+            subject=message_subject,
+            message=message_text,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=email_list,
+            fail_silently=False,
+        )
+    except smtplib.SMTPException as e:
+        server_response = e
+    return server_response
